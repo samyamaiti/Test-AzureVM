cabbd318e0152c885a0ea00a424c0378d3a3c297
Samya Maiti <samya@MacBook-Pro.local>
2021-06-21 16:47:42 +0530

    forcing build

--- main.tf
+++ main.tf
@@ -11,13 +11,41 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network1"
+  resource_group_name = var.rg_name
+  location            = var.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal1"
+  resource_group_name  = var.rg_name
+  virtual_network_name = "example-network1"
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic1"
+  location            = var.location
+  resource_group_name = var.rg_name
+
+  ip_configuration {
+    name                          = "internal1"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
 resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-machine"
+  name                = "example-machine1"
   resource_group_name = var.rg_name
   location            = var.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
 
   # admin_ssh_key {
   #   username   = "adminuser"
