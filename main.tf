0589427918a843ee8d2b506c081dabd95541bf30
Samya Maiti <samya@MacBook-Pro.local>
2021-06-21 20:21:29 +0530

    forcing build

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network1"
+  name                = "example-network"
   resource_group_name = var.rg_name
   location            = var.location
   address_space       = ["10.0.0.0/16"]
@@ -21,24 +21,24 @@ resource "azurerm_virtual_network" "example" {
 resource "azurerm_subnet" "example" {
   name                 = "internal1"
   resource_group_name  = var.rg_name
-  virtual_network_name = "example-network1"
+  virtual_network_name = "example-network"
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
-  name                = "example-nic1"
+  name                = "example-nic"
   location            = var.location
   resource_group_name = var.rg_name
 
   ip_configuration {
-    name                          = "internal1"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-machine1"
+  name                = "example-machine"
   resource_group_name = var.rg_name
   location            = var.location
   size                = "Standard_F2"
@@ -48,9 +48,15 @@ resource "azurerm_linux_virtual_machine" "example" {
   ]
 
   # admin_ssh_key {
-  #   username   = "adminuser"
-  #   public_key = file("~/.ssh/id_rsa.pub")
-  # }
+  #    username   = "adminuser"
+  #    public_key = file("~/.ssh/id_rsa.pub")
+  #  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "adminuser"
+    admin_password = "Password1234!"
+  }
 
   os_disk {
     caching              = "ReadWrite"
